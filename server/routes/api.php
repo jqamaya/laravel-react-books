55a1d375efdbf1917860e7e3f4a10e0b913697d4
Jacquelyn Amaya <amaya.jacque@gmail.com>
2024-06-03 16:10:53 +0800

    build: upgraded laravel to version 11 - due to laravel 6 not running the tests

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,32 +2,16 @@
 
 use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
-
-// Route::get('/books', 'BookController@index');
-// Route::post('/books/create', 'BookController@store');
-// Route::put('/books/update', 'BookController@store');
-// Route::get('/books/create', 'BookController@store');
-// Route::post('/books/create', 'BookController@store');
+})->middleware('auth:sanctum');
 
 Route::prefix('books')->group(function () {
-    Route::get('/', 'BookController@index');
-    Route::post('/', 'BookController@store');
-    Route::get('/{id}', 'BookController@show');
-    Route::put('/{id}', 'BookController@update');
-    Route::delete('/{id}', 'BookController@destroy');
+    Route::get('/', [BookController::class, 'index']);
+    Route::post('/', [BookController::class, 'store']);
+    Route::get('/{id}', [BookController::class, 'show']);
+    Route::put('/{id}', [BookController::class, 'update']);
+    Route::delete('/{id}', [BookController::class, 'destroy']);
 });
